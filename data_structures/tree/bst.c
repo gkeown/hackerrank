09d7b37a3acc12a8efa1090bfc0f69029c7a78db
Gerard Keown <gerardkeown@hotmail.co.uk>
2017-12-21 09:52:56 -0800

    Add BST detection, inoorder successor and lowest common ancestor exercises

--- data_structures/tree/bst.c
+++ data_structures/tree/bst.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "../common/tree.h"
+#include <limits.h>
 
 TNode *
 search(TNode *node, int value) {
@@ -158,6 +159,124 @@ find_before_after_value(TNode *root, int value, TNode **pre, TNode **suc)
     }
 }
 
+int
+isBSTUtil(TNode* node, int min, int max)
+{
+	/* an empty tree is BST */
+	if (node == NULL) {
+		return 1;
+	}
+
+    printf("check %d > %d and < %d\n", node->data, min, max);
+
+    /* false if this node violates the min/max constraint */
+    if (node->data < min || node->data > max) {
+        return 0;
+    }
+
+    /* otherwise check the subtrees recursively,
+       tightening the min or max constraint */
+    return ((isBSTUtil(node->left, min, node->data - 1)) &&  // Allow only distinct values
+            (isBSTUtil(node->right, node->data + 1, max)));  // Allow only distinct values
+}
+
+int
+isBST(TNode *node)
+{
+	return (isBSTUtil(node, INT_MIN, INT_MAX));
+}
+
+/* this is the wrong way to do this */
+int
+check_is_bst(TNode *root) {
+
+    int is_bst = 1;
+
+    /* empty tree is a BST */
+    if (root == NULL) {
+        return (1);
+    }
+
+    if (root->left != NULL) {
+        if (root->data > root->left->data) {
+            /* tree is still valid, check the next node */
+            is_bst = check_is_bst(root->left);
+            if (!is_bst) {
+                return (0);
+            }
+        } else {
+            /* not a BST */
+            return (0);
+        }
+    }
+
+    /* only check the right node if the left node is still valid */
+    if (root->right != NULL) {
+        if (root->data <= root->right->data) {
+            /* tree is still valid, check the next node */
+            is_bst = check_is_bst(root->right);
+            if (!is_bst) {
+                return (0);
+            }
+        } else {
+            /* not a BST */
+            return (0);
+        }
+    }
+
+    return (is_bst);
+}
+
+TNode *
+get_successor(TNode *root, TNode *node)
+{
+    /* sucessor in node's right branch */
+    if (node->right) {
+       return (find_smallest_value(node->right)); 
+    }
+
+    TNode *succ = NULL;
+
+    /* set succ to current root - this will change if other branches are closer to value */
+    while (root != NULL) {
+        if (node->data < root->data) {
+            /* value is in left branch */
+            printf("%d < %d, update successor to root\n", node->data, root->data);
+            succ = root;
+            root = root->left;
+        } else if (node->data >= root->data) {
+            /* value is in right branch */
+            printf("%d >= %d\n", node->data, root->data);
+            root = root->right;
+        }
+    }
+
+    return (succ);
+}
+
+TNode *
+lowest_common_ancestor(TNode *root, int value1, int value2)
+{
+    TNode *lca = root;
+
+    while (root != NULL) {
+        if (value1 < root->data && value2 < root->data) {
+            /* value is in left branch */
+            lca = root;
+            root = root->left;
+        } else if (value1 >= root->data && value2 >= root->data) {
+            /* value is in right branch */
+            lca = root;
+            root = root->right;
+        } else {
+            /* values have taken different branches */
+            break;
+        }
+    }
+
+    return (lca);
+}
+
 void
 test_bst1(void)
 {
@@ -220,4 +339,100 @@ test_bst2(void)
 
     find_before_after_value(root, 35, &pre1, &suc1);
 	printf("35 - pre: %d suc: %d\n", (pre1) ? pre1->data : -1, (suc1) ? suc1->data : -1);
+}
+
+/* https://www.geeksforgeeks.org/a-program-to-check-if-a-binary-tree-is-bst-or-not/ */
+void
+test_bst3(void)
+{
+   /* Let us create following BST
+              50
+           /     \
+          30      70
+         /  \    /  \
+       20   40  60   80 */
+    TNode *root = NULL;
+    root = insert(root, 50);
+    insert(root, 30);
+    insert(root, 20);
+    insert(root, 40);
+    insert(root, 70);
+    insert(root, 60);
+    insert(root, 80);
+
+    printf("Tree:\n");
+    print_levels(root);
+
+    int res = isBST(root);
+    printf("Tree is %s BST.\n", (res) ? "a" : "not a");
+    
+    
+    /* create a non BST */ 
+    TNode *non_bst = new_node(40);
+    non_bst->left = new_node(30);
+    non_bst->right = new_node(50);
+    non_bst->left->left = new_node(20);
+    non_bst->left->right = new_node(60);
+
+    printf("Tree:\n");
+    print_levels(non_bst);
+
+    res = isBST(non_bst);
+    printf("Tree is %s BST.\n", (res) ? "a" : "not a");
+}
+
+/* https://www.geeksforgeeks.org/inorder-successor-in-binary-search-tree/ */
+void
+test_bst4(void)
+{
+    TNode *root = NULL, *temp;
+
+    //creating the tree given in the above diagram
+    root = insert(root, 20);
+    root = insert(root, 8);
+    root = insert(root, 22);
+    root = insert(root, 4);
+    root = insert(root, 12);
+    root = insert(root, 10);  
+    root = insert(root, 14);    
+    temp = root->left->right->right; 
+
+    printf("Tree:\n");
+    print_levels(root);
+
+    TNode *succ = get_successor(root, temp);
+    if (succ) {
+        printf("Successor of %d = %d\n", temp->data, succ->data);
+    }
+
+    temp = root->left->left;
+    succ = get_successor(root, temp);
+    if (succ) {
+        printf("Successor of %d = %d\n", temp->data, succ->data);
+    }
+}
+
+void
+test_bst5(void)
+{
+    // Let us construct the BST shown in the above figure
+    TNode *root              = new_node(20);
+    root->left               = new_node(8);
+    root->right              = new_node(22);
+    root->left->left         = new_node(4);
+    root->left->right        = new_node(12);
+    root->left->right->left  = new_node(10);
+    root->left->right->right = new_node(14);
+
+    int n1 = 10, n2 = 14;
+    TNode *res = lowest_common_ancestor(root, n1, n2);
+    printf("LCA of %d and %d is %d \n", n1, n2, res->data);
+
+    n1 = 14, n2 = 8;
+    res = lowest_common_ancestor(root, n1, n2);
+    printf("LCA of %d and %d is %d \n", n1, n2, res->data);
+
+    n1 = 10, n2 = 22;
+    res = lowest_common_ancestor(root, n1, n2);
+    printf("LCA of %d and %d is %d \n", n1, n2, res->data);
 }
--- data_structures/tree/main.c
+++ data_structures/tree/main.c
@@ -4,6 +4,9 @@ void test_binary_tree(void);
 void test_diameter(void);
 void test_bst1(void);
 void test_bst2(void);
+void test_bst3(void);
+void test_bst4(void);
+void test_bst5(void);
 
 int
 main(int argc, char *argv[])
@@ -11,5 +14,8 @@ main(int argc, char *argv[])
     // test_binary_tree();
     // test_diameter();
     // test_bst1();
-    test_bst2();
+    // test_bst2();
+    // test_bst3();
+    // test_bst4();
+    test_bst5();
 }
